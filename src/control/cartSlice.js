cda91840a9e16ac67ca0d02962a7ceada8d9cde8
Hüseyin ABACI <huseyin.abaci@hotmail.com>
2024-06-22 16:40:07 +0300

    Final

---
+++ src/control/cartSlice.js
@@ -0,0 +1,49 @@
+import {createSlice} from '@reduxjs/toolkit';
+import courseItems from '../courseItems';
+
+const initialState = {
+    cartItems: courseItems,
+    quantity: 5,
+    total: 0,
+}
+
+const cartSlice = createSlice({
+    name:'cart',
+    initialState,
+    reducers:{
+        clearCart: (state) => {
+            state.cartItems = [];
+        },
+        removeItem: (state, action) => {
+            console.log(action);
+            const itemId = action.payload;
+            state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
+        },
+        increase: (state, action) => {
+            const cardItem = state.cartItems.find(
+                (item) => item.id === action.payload
+            );
+            cardItem.quantity += 1;
+        },
+        decrease: (state, action) => {
+            const cardItem = state.cartItems.find(
+                (item) => item.id === action.payload
+            )
+            cardItem.quantity -= 1;
+        },
+        calculateTotal: (state) => {
+            let total = 0;
+            let quantity = 0;
+            state.cartItems.forEach((item) => {
+                total += item.quantity * item.price;
+                quantity += item.quantity;
+            });
+            state.quantity = quantity;
+            state.total = total;
+        }
+    }
+})
+
+export const {clearCart, removeItem, increase, decrease, calculateTotal} = cartSlice.actions;
+
+export default cartSlice.reducer;
